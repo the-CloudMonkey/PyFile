4d7b02e7f03d52590acaf632ca20be75168ad72d
the-CloudMonkey <ninudayal@gmail.com>
2025-03-17 17:04:23 +0530

    Create main.py
    python script

---
+++ main.py
@@ -0,0 +1,8 @@
+import time
+
+print ("welcome to Niharika's python code")
+x=input("Enter the value of x:")
+print(x)
+time.sleep(5)
+print("Thank you for using Niharika's python code")
+time.sleep(3)
